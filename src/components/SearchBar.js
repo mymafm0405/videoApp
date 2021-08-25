a83fa03270341b3b21ddb520f52d8e7b8f9000b8
Mahmoud <mymafm0405@gmail.com>
2021-08-25 19:51:32 +0300

    add some style to the input form

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,16 @@ import React from "react";
 
 class SearchBar extends React.Component {
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <div className="ui segment">
+        <form className="ui form">
+          <div className="field">
+            <labe>Video search:</labe>
+            <input type="textt" />
+          </div>
+        </form>
+      </div>
+    );
   }
 }
 
